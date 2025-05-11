2f9b805b65acdc1483491c27c16ae55c547eab99
VictorJJSJR <131430262+VictorJJSJR@users.noreply.github.com>
2025-05-10 18:11:24 -0700

    (V) Se arreglo el problema que existia de rutas de Area, Catalogos y Explorar

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import json
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="app/templates")
 
 # Ruta al JSON generado por el scraper
 RUTA_JSON = os.path.join("datos", "json", "revistas_completo.json")
@@ -28,6 +28,7 @@ def revistas_por_area(nombre):
     filtradas = {titulo: datos for titulo, datos in revistas.items() if nombre in datos.get("areas", [])}
     return render_template("revistas_filtradas.html", tipo="Área", filtro=nombre, revistas=filtradas)
 
+
 @app.route("/catalogos")
 def mostrar_catalogos():
     catalogos = set()
@@ -41,15 +42,17 @@ def revistas_por_catalogo(nombre):
     filtradas = {titulo: datos for titulo, datos in revistas.items() if nombre in datos.get("catalogos", [])}
     return render_template("revistas_filtradas.html", tipo="Catálogo", filtro=nombre, revistas=filtradas)
 
+
 @app.route("/explorar")
 def explorar():
     letras = sorted(set(titulo[0].upper() for titulo in revistas))
     return render_template("explorar.html", letras=letras)
 
 @app.route("/explorar/<letra>")
-def revistas_por_letra(letra):
+def explorar_letra(letra):
     filtradas = {titulo: datos for titulo, datos in revistas.items() if titulo.lower().startswith(letra.lower())}
     return render_template("revistas_letra.html", letra=letra, revistas=filtradas)
+
 
 @app.route("/revista/<titulo>")
 def detalle_revista(titulo):
